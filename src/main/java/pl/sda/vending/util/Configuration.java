839ac17061b291b60ccba3aafe4e7685a3204676
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-03 12:04:12 +0100

    - reading conf from properties

---
+++ src/main/java/pl/sda/vending/util/Configuration.java
@@ -0,0 +1,28 @@
+package pl.sda.vending.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration {
+    private final Properties properties;
+
+    public Configuration() {
+        properties = new Properties();
+        try (InputStream propertiesStream = ClassLoader
+                .getSystemResourceAsStream("application.properties")) {
+            properties.load(propertiesStream); // po try jest od razu close(), JVM gwarantuje
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        properties.list(System.out); // wypisanie wszystkich
+    }
+
+    public Long getLongProperty(String paramName, Long defaultValue) {
+        final String propertyValue = properties.getProperty(paramName);
+        return propertyValue != null ? Long.parseLong(propertyValue) : defaultValue;
+    }
+
+    public String getStringProperty(String paramName, String defaultValue) {
+        return properties.getProperty(paramName, defaultValue);
+    }
+}
